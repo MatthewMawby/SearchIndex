55c13b2706cb770675f7a705d12390a262dd1206
Matthew Mawby <matthew@Matthews-MBP.nycap.rr.com>
2017-11-27 14:05:43 -0500

    Added funnctionality to document_controller

--- src/python/utils/document_controller.py
+++ src/python/utils/document_controller.py
@@ -9,17 +9,35 @@ class DocumentController(object):
 
     def __init__(self):
         self._dynamodb = boto3.client('dynamodb')
-        self.document_table = (boto3.resource('dynamodb')
-                                  .Table(self.DOCUMENT_TABLE))
+        self._document_table = (boto3.resource('dynamodb')
+                                .Table(self.DOCUMENT_TABLE))
 
+    # retrieves the document if present, None if not present or failure
+    def get_document(self, doc_id):
+        try:
+            res = self._document_table.query(
+                KeyConditionExpression=Key(DocumentModel.PKEY).eq(doc_id)
+            )
+            if res['Count'] == 0:
+                return None
+            return res['Items'][0]
+        except Exception as ex:
+            print "ERROR: Failed to query {0} \
+            table: {1}".format(self.DOCUMENT_TABLE, ex)
+            return None
 
     # create a new index document if the provided model is valid
     def create_new_document(self, document_model):
         if not document_model.verify():
-            return
-        self._index_table.put_item(
-            Item=index_model.get_payload()
-        )
+            return False
+        try:
+            self._document_table.put_item(
+                Item=document_model.get_payload()
+            )
+            return True
+        except Exception as ex:
+            print "ERROR: Failed to create new document: {0}".format(ex)
+            return False
 
     # acquire a lock on the document
     def lock_document(self, doc_id):
