695784927ebf842331777f190fe95e5fd1a742fd
Matthew Mawby <themesta@gmail.com>
2017-11-28 05:34:43 -0500

    Added locking to document_controller

--- src/python/utils/document_controller.py
+++ src/python/utils/document_controller.py
@@ -1,9 +1,15 @@
 import boto3
+from datetime import datetime
 from boto3.dynamodb.conditions import Key, Attr
 from document_model import DocumentModel
 
 
 class DocumentController(object):
+    '''
+    This class implements the data access for the DOCUMENTS table. It is
+    responsible for all operations with the table including creating documents,
+    reading documents, and modifying the state of documents.
+    '''
 
     DOCUMENT_TABLE = 'DOCUMENTS'
 
@@ -26,6 +32,41 @@ class DocumentController(object):
             table: {1}".format(self.DOCUMENT_TABLE, ex)
             return None
 
+    # set updating field to true & set lastUpdate timestamp
+    def lock_document(self, doc_id):
+        now = str(datetime.now())
+        update_expression = 'set {0}.{1} = :updating, {2}.{3} = :now'.format(
+            DocumentModel.INTERNAL,
+            DocumentModel.UPDATING,
+            DocumentModel.INTERNAL,
+            DocumentModel.LAST_UPDATE)
+        try:
+            res = self._document_table.update_item(Key={
+                DocumentModel.PKEY: doc_id
+            }, UpdateExpression=update_expression, ExpressionAttributeValues={
+                ':updating': True,
+                ':now': now
+            })
+        except Exception as ex:
+            print "ERROR: Failed to acquire lock on document"
+            raise ex
+
+    # set updating field to false for given document
+    def unlock_document(self, doc_id):
+        now = str(datetime.now())
+        update_expression = 'set {0}.{1} = :updating'.format(
+            DocumentModel.INTERNAL,
+            DocumentModel.UPDATING)
+        try:
+            res = self._document_table.update_item(Key={
+                DocumentModel.PKEY: doc_id
+            }, UpdateExpression=update_expression, ExpressionAttributeValues={
+                ':updating': False
+            })
+        except Exception as ex:
+            print "ERROR: Failed to unlock document"
+            raise ex
+
     # create a new index document if the provided model is valid
     def create_new_document(self, document_model):
         if not document_model.verify():
@@ -38,7 +79,3 @@ class DocumentController(object):
         except Exception as ex:
             print "ERROR: Failed to create new document: {0}".format(ex)
             return False
-
-    # acquire a lock on the document
-    def lock_document(self, doc_id):
-        return "hi"
