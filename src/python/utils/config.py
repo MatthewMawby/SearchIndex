bfad94dbc7403bd20ba3c5968d284ed22e3e2d6b
Matthew Mawby <themesta@gmail.com>
2017-12-04 10:20:28 -0500

    Added config.py to store configurable constants

---
+++ src/python/utils/config.py
@@ -0,0 +1,7 @@
+class Config(object):
+    WRITE_STREAM = 'writeMastertoWorker'
+    SEARCH_STREAM = 'searchMastertoWorker'
+    WRITE_MESSAGES = 'writeServiceQueue'
+    SEARCH_MESSAGES = 'searchServiceQueue'
+    INDEX_MAX_SIZE = 1000
+    FILE_DIRECTORY = '/tmp/'
