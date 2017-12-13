a3a641ba467b61b742f4ce0fe6b3f694aa17e2c4
Matthew Mawby <themesta@gmail.com>
2017-12-13 03:35:36 -0500

    Added deployment config for stopword generation service

--- src/python/stopword_generation/stopword_generator.py
+++ src/python/stopword_generation/stopword_generator.py
@@ -6,6 +6,7 @@ from stopword_model import StopWordModel
 
 import boto3
 
+
 class StopWordGenerator:
 
     def __init__(self):
@@ -18,7 +19,7 @@ class StopWordGenerator:
     '''
     Search a partition and update all_tokens with a given version
     '''
-    def search_partition(self,key):
+    def search_partition(self, key):
         # lookup partition using key
         payload = self._storage.get_partition(key)
         # deserialize key
@@ -29,12 +30,13 @@ class StopWordGenerator:
 
         for key in self.partition.get_token_list():
             addamount = self.partition.get_token_count(key)
-            if(addamount<=0):
+            if addamount <= 0:
                 continue
             if(key in self._tokens.keys()):
                 self._tokens[key] += addamount
             else:
                 self._tokens[key] = addamount
+
     '''
     generate stop words by searching through all partitions
     '''
@@ -42,14 +44,16 @@ class StopWordGenerator:
         partition_ids = self._index.get_partition_ids()
         for k in partition_ids:
             self.search_partition(k)
+
     '''
     iterate through all tokens and add to stopwords table
     '''
     def add_stopwords_table(self):
         for t in self._tokens.keys():
             sw = StopWordModel()
-            sw.set_data(t,self._tokens[t])
+            sw.set_data(t, self._tokens[t])
             self._stopword.add_word(sw)
+
     '''
     return all stop words as dictionary
     '''
